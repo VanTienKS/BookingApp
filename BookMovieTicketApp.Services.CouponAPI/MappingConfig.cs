197f8bed8bfa76513c15ac7c2f351fdb8685b5c6
VanTienPY <130162937+VanTienKS@users.noreply.github.com>
2024-10-17 08:59:23 +0700

    basic

---
+++ BookMovieTicketApp.Services.CouponAPI/MappingConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BookMovieTicketApp.Services.CouponAPI.Models;
+using BookMovieTicketApp.Services.CouponAPI.Models.Dto;
+
+namespace BookMovieTicketApp.Services.CouponAPI {
+    public class MappingConfig {
+        public static MapperConfiguration RegisterMap() {
+            var mappingCongif = new MapperConfiguration(config => {
+                config.CreateMap<CouponDto, Coupon>();
+                config.CreateMap<Coupon, CouponDto>();
+            });
+            return mappingCongif;
+        }
+    }
+}
---
+++ BookMovieTicketApp.Services.CouponAPI/Models/Dto/CouponDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BookMovieTicketApp.Services.CouponAPI.Models.Dto {
+    public class CouponDto {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
