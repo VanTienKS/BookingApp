d3dce884de462a43f1c552c6530c427c4ba62611
VanTienPY <130162937+VanTienKS@users.noreply.github.com>
2024-10-18 16:46:17 +0700

    a

--- BookMovieTicketApp.Services.CouponAPI/obj/Debug/net8.0/BookMovieTicketApp.Services.CouponAPI.AssemblyInfo.cs
+++ BookMovieTicketApp.Services.CouponAPI/obj/Debug/net8.0/BookMovieTicketApp.Services.CouponAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookMovieTicketApp.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a79d903a1fa0cd3b82df5d4a7f62ce3801f53c44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e90f849d0066a55bbc78ca3b301d136f91aba64")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookMovieTicketApp.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookMovieTicketApp.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
